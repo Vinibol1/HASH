264697300d3a128241ed5621c61a43d3f7aca128
Vini <tportella@fi.uba.ar>
2024-11-08 02:02:04 -0300

    V0.0.2

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -9,10 +9,60 @@ void hash_crear_pruebas()
 	hash_destruir(hash);
 }
 
+void hash_insertar_pruebas()
+{
+	hash_t *hash = hash_crear(4);
+	char *clave = "daa";
+	int valor = 2;
+	pa2m_afirmar(hash_insertar(hash,clave,&valor,NULL) == true, "hash devolvio true");
+	pa2m_afirmar(hash_cantidad(hash) == 1, "La función has empieza con 0 elementos");
+	pa2m_afirmar(hash_contiene(hash,clave) == true, "La función encontro el elemento correctamente");
+	hash_destruir(hash);
+}
+
+void hash_insertar_pruebas2()
+{
+	hash_t *hash = hash_crear(4);
+	char *clave = "daa";
+	int valor = 2;
+	char *clave2 = "asd0";
+	int valor2 = 2;
+	char *clave3 = "asd1";
+	int valor3 = 2;
+	char *clave4 = "asd2";
+	int valor4 = 2;
+	char *clave5 = "asd3";
+	int valor5 = 2;
+	pa2m_afirmar(hash_insertar(hash,clave,&valor,NULL) == true, "hash devolvio true");
+	pa2m_afirmar(hash_cantidad(hash) == 1, "La función tiene 1 elemento");
+	pa2m_afirmar(hash_insertar(hash,clave2,&valor2,NULL) == true, "hash devolvio true");
+	pa2m_afirmar(hash_cantidad(hash) == 2, "La función tiene  2 elementos");
+	pa2m_afirmar(hash_insertar(hash,clave3,&valor3,NULL) == true, "hash devolvio true");
+	pa2m_afirmar(hash_cantidad(hash) == 3, "La función tiene  3 elementos");
+	pa2m_afirmar(hash_insertar(hash,clave4,&valor4,NULL) == true, "hash devolvio true");
+	pa2m_afirmar(hash_cantidad(hash) == 4, "La función tiene  4 elementos");
+	pa2m_afirmar(hash_insertar(hash,clave5,&valor5,NULL) == true, "hash devolvio true");
+	pa2m_afirmar(hash_cantidad(hash) == 5, "La función tiene  4 elementos");
+	pa2m_afirmar(hash_contiene(hash,clave) == true, "La función encontro la clave correctamente");
+	pa2m_afirmar(hash_contiene(hash,clave2) == true, "La función encontro la clave correctamente");
+	pa2m_afirmar(hash_contiene(hash,clave3) == true, "La función encontro la clave correctamente");
+	pa2m_afirmar(hash_contiene(hash,clave4) == true, "La función encontro la clave correctamente");
+	pa2m_afirmar(hash_contiene(hash,"aguante racing") == false, "le paso una clave que no existe devuelve false");
+	pa2m_afirmar(*(int*)hash_buscar(hash,clave) == valor, "La función encontro el elemento correctamente");
+	pa2m_afirmar(*(int*)hash_buscar(hash,clave2) == valor2, "La función encontro el elemento correctamente");
+	pa2m_afirmar(*(int*)hash_buscar(hash,clave4) == valor3, "La función encontro el elemento correctamente");
+	pa2m_afirmar(*(int*)hash_buscar(hash,clave3) == valor4, "La función encontro el elemento correctamente");
+	pa2m_afirmar((int*)hash_buscar(hash,"asdasd") == NULL, "le paso una clave que no existe y devuelve NULL");
+	hash_destruir(hash);
+}
+
+
 int main()
 {
 	pa2m_nuevo_grupo("Hash crear");
 	hash_crear_pruebas();
+	hash_insertar_pruebas();
+	hash_insertar_pruebas2();
 
 	return pa2m_mostrar_reporte();
 }
--- src/hash.c
+++ src/hash.c
@@ -1,4 +1,5 @@
 #include "stdlib.h"
+#include "stdio.h"
 #include "string.h"
 #include "hash.h"
 #include "abb.h"
@@ -39,58 +40,105 @@ size_t hash_cantidad(hash_t* hash){
         if (!hash)
                 return 0;
         
-        size_t contador;
+        size_t contador = 0;
         for (size_t i = 0; i < hash->capacidad; i++)
                 contador += abb_cantidad(hash->tabla[i]);
         return contador;
 }
 
-static size_t funcion_hash(char *clave){
-        return 0;
-}
+static size_t funcion_hash(unsigned char *str){
+        unsigned long hash = 5381;
+        unsigned long c = *str++;
 
-bool hash_insertar(hash_t* hash, char* clave, void* valor, void** encontrado){
-        if (!hash || !clave)
-                return false;
-        size_t indice = funcion_hash(clave) % hash->capacidad;
-        char copia_clave;
-        strcpy(&copia_clave,clave);
-        pares_t *pares = calloc(1,sizeof(pares_t));
-        pares->clave = &copia_clave;
-        pares->valor = valor; 
-        return abb_insertar(hash->tabla[indice],pares);
+        while (c){
+                hash = ((hash << 5) + hash*c) + c;
+                c = *str++;
+        }
+        return hash;
 }
 
 
 void* hash_buscar(hash_t* hash, char* clave){
         if (!hash || !clave)
                 return NULL;
-        for (size_t i = 0; i < hash->capacidad; i++)
-                return abb_obtener(hash->tabla[i],clave);
+        size_t indice = funcion_hash((unsigned char *)clave) % hash->capacidad;
+        pares_t pares;
+        pares_t *pares2 = NULL;
+        pares.clave = clave;
+        pares2 = (pares_t*)abb_obtener(hash->tabla[indice],&pares);
+        if (pares2)
+                return pares2->valor;
+        
         return NULL;
 }
 
 bool hash_contiene(hash_t* hash, char* clave){
         if (!hash || !clave)
                 return false;
-        for (size_t i = 0; i < hash->capacidad; i++)
-                if(abb_obtener(hash->tabla[i],clave) != NULL)
-                        return true;       
+        size_t indice = funcion_hash((unsigned char *)clave) % hash->capacidad;
+        pares_t pares;
+        pares.clave = clave;
+        if( abb_obtener(hash->tabla[indice],&pares) != NULL)
+                return true;       
         return false;
 }
+
+bool modificar_valor_par(void *elemento, void *ctx){
+        pares_t *pares_elem = elemento;
+        pares_t *pares = ctx;
+        if (pares_elem->clave == pares->clave){
+                pares_elem->valor = pares->valor;
+                return false;
+        }
+        return true;
+}
+
+
+bool hash_insertar(hash_t* hash, char* clave, void* valor, void** encontrado){
+        if (!hash || !clave)
+                return false;
+        size_t indice = funcion_hash((unsigned char *)clave) % hash->capacidad;
+        char *copia_clave = calloc(strlen(clave) + 1,sizeof(char));
+        for(int i = 0; i < strlen(clave) + 1;i++)
+                copia_clave[i] = clave[i];
+        pares_t pares2;
+        pares2.clave = copia_clave;
+        pares2.valor = valor; 
+        if (encontrado && hash_contiene(hash,clave)){
+                abb_iterar_inorden(hash->tabla[indice],modificar_valor_par,&pares2);
+        }
+        pares_t *pares = calloc(1,sizeof(pares_t));
+        pares->clave = copia_clave;
+        pares->valor = valor; 
+
+        
+        return abb_insertar(hash->tabla[indice],pares);
+}
+
 
 size_t hash_iterar(hash_t* hash, bool (*f)(char*, void*, void*), void* ctx){
         return 0;
 }
 
+static bool destruir_claves(void *pares_, void *ctx){
+        pares_t *pares = pares_;
+        free(pares->clave);
+        return true;
+}
 
+static void destruir_pares(void *pares_){
+        pares_t *pares = pares_;
+        free(pares);
+}
 
 void hash_destruir(hash_t* hash){
         if (!hash)
-                return
+                return;
         
-        for (size_t i = 0; i < hash->capacidad; i++)
-                abb_destruir(hash->tabla[i]);
+        for (size_t i = 0; i < hash->capacidad; i++){
+                abb_iterar_inorden(hash->tabla[i],destruir_claves,NULL);
+                abb_destruir_todo(hash->tabla[i],destruir_pares);
+        }
         free(hash->tabla);
         free(hash);
 }
@@ -99,8 +147,10 @@ void hash_destruir_todo(hash_t* hash, void (*destructor)(void*)){
         if (!hash)
                 return;
                 
-        for (size_t i = 0; i < hash->capacidad; i++)
+        for (size_t i = 0; i < hash->capacidad; i++){
+                abb_iterar_inorden(hash->tabla[i],destruir_claves,NULL);
                 abb_destruir_todo(hash->tabla[i],destructor);
+                }
         free(hash->tabla);
         free(hash);
 }
